9d8b44e64f76970e11b23ff266f342893cc548aa
Michael Randall <m_ran66@hotmail.com>
2021-04-10 15:33:10 -0700

    added lazy load capability to migrationpage.js

--- src/components/migrations/MigrationsPage.js
+++ src/components/migrations/MigrationsPage.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 
-import MigrationList from "./MigrationList";
+const MigrationList = React.lazy(() => import("./MigrationList"));
 
 const MigrationsPage = () => {
   return (
     <>
+    <Suspense fallback={<h2>Loading...</h2>}>
       <MigrationList />
+    </Suspense>
     </>
   );
 };
